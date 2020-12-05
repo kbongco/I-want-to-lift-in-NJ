a82b856264aaad10e6ec540589c5a42c08f3602e
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-12-05 14:33:49 -0500

    fixed contact form to go to airtable

--- src/Screens/About/About.jsx
+++ src/Screens/About/About.jsx
@@ -14,10 +14,25 @@ export default function About() {
           location that I may have missed, please contact me and I'll add it
           accordingly.
         </p>
+
+        <p>What determines what gets into the finder?</p>
+        <p>Yes, Yes I know there are a lot of "commercial" gyms in the area. But this 
+        is specifically for powerlifting/weightlifting friendly gyms. How I determine
+        what gets listed, is based on pretty much on the people I follow on IG and where
+        they train at. Not the best critera, but if I see someone I know who's a powerlifter and training at the gym that's 
+        enough criteria for me. 
+         
+        </p>
+        <p>My gym isn't listed, how do I get in contact with you?</p>
+        There's a contact form linked above. Fill it out and I'll get back to you 
+        as soon as I can. 
+        Also, for the programming background, I used the Yelp API to help me with this 
+        for some reason some places that I know that are weightlifting/powerlifting friendly 
+        isn't listed in the Yelp API so that may also be the reason why that gym isn't listed. 
         <br />
         <p>
           Also, this is technically a "mobile" app, so please view on your
-          various mobile devices for the best/optimized quality
+          various mobile devices for the best/optimized quality.
         </p>
       </div>
     </>
--- src/Screens/ContactForm/Contact.jsx
+++ src/Screens/ContactForm/Contact.jsx
@@ -1,74 +1,81 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
 export default function Contact() {
-  const [state, updateState] = useState({
-    name: '',
-    email: '',
-    subject: '',
-    message:''
-  })
+ 
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [subject, setSubject] = useState('')
+  const [message, setMessage] = useState('')
 
-  const sent = e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-  }
-
-  const onInputChange = e => {
-    const { name, value } = e.target 
-    
-    updateState({
-      ...state,
-      [name]: value
-    });
-  }
+    const contacted = await axios.post(
+      "https://api.airtable.com/v0/app2PwmQ9IIxH0rhV/Table%201",
+      {
+        fields: {
+          name: name,
+          email: email,
+          subject: subject,
+          message: message
+        },
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  };
 
   return (
-    <div className='form'>
-      <h1>Contact me!</h1>
-      <p>Got some feedback about the design? Want a gym to be added on here? or maybe you just want to say hi? Drop an email down below and I'll get back to you ASAP!</p>
-      <h1>Form is still under construction :) </h1>
-      <form className='contact' onSubmit={sent}>
-        <label htmlFor='Name'>Name</label><br/>
-        <input 
-          type='text'
-          id='name'
-          value={state.name}
-          placeholder="Your name here"
-          onChange={onInputChange}
+    <div class="form">
+      <form onSubmit={handleSubmit}>
+        <h2>Contact me!</h2>
+        <p>
+          Got a gym you want me to add into this list? Have some feedback about the design? 
+          Want to help me out with this once I turn this into a long-term project? 
+          Drop me a message and I'll get back to you ASAP
+        </p>
+        <label htmlFor="name">Name</label>
+        <br></br>
+        <input
+          type="text"
+          id="Name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
         />
-        <br/>
-        <label htmlFor='email'>Email</label><br />
-        <input 
-          type='text'
-          id='email'
-          value={state.email}
-          placeholder='Your email here'
-          onChange={onInputChange}
+        <br></br>
+
+        <label htmlFor="email">email</label>
+        <input
+          type="url"
+          name="url"
+          id="url"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
         />
-        <br />
-        <label htmlFor='subject'>Subject</label><br />
-        <input 
+        <br></br>
+
+        <label htmlFor='subject'>subject</label>
+        <input
           type='text'
           id='subject'
-          value={state.subject}
-          placeholder='Why you wanna email me?'
-          onChange={onInputChange}
+          onChange={(e) => setSubject(e.target.value)}
+          value={subject}
         />
         <br />
-        <label htmlFor='message'>Message</label><br />
-        <textarea
+        
+        <label htmlFor='message'>message</label>
+        <input 
           type='textarea'
-          name='message'
-          value={state.message}
-          rows='10'
-          placeholder='Whats on your mind?'
-          onChange={onInputChange}
-        />
-        <br />
-        <button className='emailme' type='submit'>
-          Email me!
-        </button>
+          id='message'
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          />
       </form>
+      <button type='submit'>Contact me!</button>
     </div>
-  )
-
+  );
 }
