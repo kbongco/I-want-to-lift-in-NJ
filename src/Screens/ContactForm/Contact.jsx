e3f329635729486a0c05f035506e8f6723c9585a
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-12-12 15:12:46 -0500

    removed server.js file, changed form from airtable to formspree

--- src/Screens/ContactForm/Contact.jsx
+++ src/Screens/ContactForm/Contact.jsx
@@ -1,84 +1,71 @@
-import axios from 'axios';
-import React, { useState } from 'react';
-import './Contact.css'
+import axios from "axios";
+import React, { useState } from "react";
+import "./Contact.css";
 
-export default function Contact() {
- 
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [subject, setSubject] = useState('')
-  const [message, setMessage] = useState('')
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const contacted = await axios.post(
-      "https://api.airtable.com/v0/app2PwmQ9IIxH0rhV/Table%201",
-      {
-        fields: {
-          name: name,
-          email: email,
-          subject: subject,
-          message: message
-        },
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
+function Contact() {
+  const [serverState, setServerState] = useState({
+    submitting: false,
+    status: null,
+  });
+  const handleServerResponse = (ok, msg, form) => {
+    setServerState({
+      submitting: false,
+      status: { ok, msg },
+    });
+    if (ok) {
+      form.reset();
+    }
   };
-
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    setServerState({ submitting: true });
+    axios({
+      method: "post",
+      url: "https://formspree.io/xleopplb",
+      data: new FormData(form),
+    })
+      .then((r) => {
+        handleServerResponse(
+          true,
+          "Thanks for your inquiry, I will get back to you ASAP",
+          form
+        );
+      })
+      .catch((r) => {
+        handleServerResponse(false, r.response.data.error, form);
+      });
+  };
   return (
-    <div class="form">
-      <form onSubmit={handleSubmit}>
-        <h2>Contact me!</h2>
-        <p>
-          Got a gym you want me to add into this list? Have some feedback about the design? 
-          Want to help me out with this once I turn this into a long-term project? 
-          Drop me a message and I'll get back to you ASAP
-        </p>
-        <label htmlFor="name">Name</label>
-        <br></br>
-        <input
-          type="text"
-          id="Name"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
-        <br></br>
+    <div>
+      <h1>Contact me</h1>
+      <p>
+        Got a gym you want me to add into this list? Have some feedback about
+        the design? Want to help me out with this once I turn this into a
+        long-term project? Drop me a message and I'll get back to you ASAP
+      </p>
+      <form onSubmit={handleOnSubmit}>
+        <label htmlFor="name">Name:</label>
         <br/>
-        <label htmlFor="email">email</label><br/>
-        <input
-          type="url"
-          name="url"
-          id="url"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-        <br></br>
-
-        <label htmlFor='subject'>subject</label><br/>
-        <input
-          type='text'
-          id='subject'
-          onChange={(e) => setSubject(e.target.value)}
-          value={subject}
-        />
-        <br />
-        
-        <label htmlFor='message'>message</label><br/>
-        <textarea
-          type='textarea'
-          id='message'
-          cols='30'
-          rows='5'
-          onChange={(e) => setMessage(e.target.value)}
-          value={message}
-          />
+        <input id="name" type="name" name="name" required /><br/>
+        <label htmlFor="email">Email:</label><br/>
+        <br/>
+        <input id="email" type="email" name="email" required /><br/>
+        <label htmlFor="message">Message:</label><br/>
+        <br/>
+        <textarea id="message" name="message" rows='15' ></textarea>
+        <br/>
+        <button type="submit" disabled={serverState.submitting}>
+          Submit
+        </button>
+        {serverState.status && (
+          <p className={!serverState.status.ok ? "errorMsg" : ""}>
+            {serverState.status.msg}
+          </p>
+        )}
       </form>
-      <button type='submit'>Contact me!</button>
     </div>
   );
 }
+
+export default Contact;
